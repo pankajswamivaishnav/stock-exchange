f2b425b3f1229494f394e02c60ede82f3f07f519
pankaj7073 <pankajvaishnav128@gmail.com>
2024-06-03 19:38:54 +0530

    resolve issue

--- server/config/db/connection.js
+++ server/config/db/connection.js
@@ -3,11 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://mongodb:mongodb9468@newecommercecluster.xez4vtt.mongodb.net/yourDatabaseNameHere?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      "mongodb+srv://mongodb:mongodb9468@newecommercecluster.xez4vtt.mongodb.net/yourDatabaseNameHere?retryWrites=true&w=majority"
     );
     console.log("MongoDB connected...");
   } catch (err) {
--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 8000;
 // Frontend
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 app.listen(PORT, () => {
