694ce4daaa290361495549b2a9f78a498254a5db
pankaj7073 <pankajvaishnav128@gmail.com>
2024-06-03 22:14:19 +0530

    final call

--- server/server.js
+++ server/server.js
@@ -16,6 +16,10 @@ app.use(cors());
 app.use("/api", require("./routes/orders"));
 
 const PORT = process.env.PORT || 8000;
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
